33df065578969824f93fa2baec8113630b40599a
javier <javier.rojas02@alumnos.uach.cl>
2017-05-27 15:40:22 -0400

    Add files via upload

--- casilla.h
+++ casilla.h
@@ -8,6 +8,14 @@ typedef struct nodo{
     struct nodo *a;
 
 }nodo;
+void bienvenida(){
+	for(int x=0;x<20;x++){
+		for(int y=0;y<20;y++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
 
 nodo* crear_nodo(){
 	nodo *n=(nodo *)malloc(sizeof(nodo));
@@ -89,34 +97,196 @@ void conectar_nodos(nodo** k,int n, int m){
 		}
 	}
 }
-void generar_posicion(nodo *k){
-	k->val='#';
-}
-void generar_direccion(nodo *k){
-	k->val='X';
-}
-void limpiar_casilla(nodo *k){
-	k->val='O';
-}
-void eliminar_tierra(nodo *k,nodo *t){
-	if(k->w==t){
-		printf("%s\n","si entre" );
-		k->w=t->w;
-		(t->w)->s=k;
-		t->w=NULL;
-		t->s=NULL;
+nodo* generar_posicion(nodo *k,char c){
+	if(c=='w'){
+		(k->w)->val='#';
+		return k->w;
 	}
+	else if(c=='d'){
+		(k->d)->val='#';
+		return k->d;
+	}
+	else if(c=='s'){
+		(k->s)->val='#';
+		return k->s;
+	}
+	else if(c=='a'){
+		(k->a)->val='#';
+		return k->a;
+	}
+	
+	
+}
+nodo* generar_direccion(nodo *k, nodo **j,char c, int n, int m){
+	if(c=='w'){
+	for(int x=0;x<n;x++){
+		for(int y=0;y<m;y++){
+			if(&j[x][y]==k){
+				if(x==0){
+					(&j[n-1][y])->val='x';
+					return(&j[n-1][y]);
+				}
+				else{
+					(&j[x-1][y])->val='x';
+					return(&j[x-1][y]);
+				}
+				
+			}
+		}
+	}
+	}
+	else if(c=='a'){
+		for(int x=0;x<n;x++){
+			for(int y=0;y<m;y++){
+				if(&j[x][y]==k){
+					if(y==0){
+						(&j[x][m-1])->val='x';
+						return(&j[x][m-1]);
+					}
+					else{
+						(&j[x][y-1])->val='x';
+						return(&j[x][y-1]);
+					}
+					
+				}
+			}
+		}
+	}
+	else if(c=='d'){
+		for(int x=0;x<n;x++){
+			for(int y=0;y<m;y++){
+				if(&j[x][y]==k){
+					if(y==m-1){
+						(&j[x][0])->val='x';
+						return(&j[x][0]);
+					}
+					else{
+						(&j[x][y+1])->val='x';
+						return(&j[x][y+1]);
+					}
+					
+				}
+			}
+		}
+	}
+	else if(c=='s'){
+		for(int x=0;x<n;x++){
+			for(int y=0;y<m;y++){
+				if(&j[x][y]==k){
+					if(x==n-1){
+						(&j[0][y])->val='x';
+						return(&j[0][y]);
+					}
+					else{
+						(&j[x+1][y])->val='x';
+						return(&j[x+1][y]);
+					}
+					
+				}
+			}
+		}
+	}
+
+	
+}
+void limpiar_casilla(nodo *t){
+	int g=0;
+	if(t->a==NULL){
+		g=g+1;
+	}
+	if(t->w==NULL){
+		g=g+1;
+	}
+	if(t->d==NULL){
+		g=g+1;
+	}
+	if(t->s==NULL){
+		g=g+1;
+	}
+	if(g<4){
+		t->val='O';
+	}
+	else{
+		
+		t->val=' ';
+	}
+	
+}
+void agregar_tierra(nodo *k,nodo *t,char c){
+	int g=0;
+	if(t->a==NULL){
+		g=g+1;
+	}
+	if(t->w==NULL){
+		g=g+1;
+	}
+	if(t->d==NULL){
+		g=g+1;
+	}
+	if(t->s==NULL){
+		g=g+1;
+	}
+
+	if(g>=2){
+		if(c=='w'){
+			t->w=k->w;
+			(k->w)->s=t;
+			k->w=t;
+			t->s=k;
+		}
+		else if(c=='d'){
+			t->d=k->d;
+			(k->d)->a=t;
+			k->d=t;
+			t->a=k;
+		}
+		else if(c=='s'){
+			t->s=k->s;
+			(k->s)->w=t;
+			k->s=t;
+			t->w=k;
+		}
+		else if(c=='a'){
+			t->a=k->a;
+			(k->a)->d=t;
+			k->a=t;
+			t->d=k;
+		}
+		
+	}
+	else{
+		printf("%s\n","la tierra escogida ya esta agregada " );
+		printf("presione cualquier tecla para volver a intentar...\n");
+		system("pause");
+	}
+}
+void eliminar_tierra(nodo *t){
+	nodo* arriba=t->w;
+	nodo* abajo=t->s;
+	nodo* izquierda=t->a;
+	nodo* derecha=t->d;
+	izquierda->d=derecha;
+	derecha->a=izquierda;
+	abajo->w=arriba;
+	arriba->s=abajo;
+	t->w=NULL;
+	t->a=NULL;
+	t->d=NULL;
+	t->s=NULL;
+	
 }
 void imprimir_matriz(nodo **k,int n, int m){
 	for(int x=0;x<n;x++){
 		for(int y=0;y<m;y++){
 			printf("%c",(&k[x][y])->val);
 			if(y!=m-1){
-				if((&k[x][y])->d==&k[x][y+1]){
+				if((&k[x][y])->d==	&k[x][y+1]){
 				printf("%s","-");
 				}
-			}
-			
+				else{
+					printf(" ");
+				}
+			}	
 		}
 		printf("\n" );
 		if(x!=n-1){
@@ -124,9 +294,11 @@ void imprimir_matriz(nodo **k,int n, int m){
 				if((&k[x][j])->s==&k[x+1][j]){
 				printf("%s","| ");
 				}
+				else{
+					printf("  ");
+				}
 			}
-		}
-		
+		}	
 		printf("\n" );
 		
 	}
